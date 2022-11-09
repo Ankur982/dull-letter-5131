0005de53c2e1f3bb54e5ef0404bb053046741176
Ankur982 <singh999ankur@gmail.com>
2022-11-09 23:28:31 +0530

    updaitin navbar and footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -3,37 +3,28 @@ import "./Footer.css"
 const Footer = () => {
   return (
     <>
-    <div className="footer1">
+    <div className="footer-section-1">
         <p>Website feedback? Let us know </p>
     </div>
-    <div className="footer2">
+    <div className="footer-section-2">
 
-        <div className="labelBox">
-            <div className="label-text">
+        <div className="label-container">
+            <div className="label-box">
                 <img src="https://www.sephora.com/img/ufe/icons/find-store.svg"  alt='img'/>
-
                 <div>
                     <span>Find a store</span>
                     <p>Choose Your Store</p>
                 </div>
             </div>
-            <div className="label-text">
-
-
+            <div className="label-box">
                 <img src="https://www.sephora.com/img/ufe/icons/find-store.svg" alt='img' />
-
-
                 <div>
                     <span>Live Beauty Help</span>
                     <p>Chat is unavailable</p>
                 </div>
             </div>
-            <div className="label-text">
-
-
+            <div className="label-box">
                 <img src="https://www.sephora.com/img/ufe/icons/call.svg" alt='img'/>
-
-
                 <div>
                     <span>
                         1-877-737-4672
@@ -41,26 +32,17 @@ const Footer = () => {
                     <p>TTY: 1-888-866-9845 </p>
                 </div>
             </div>
-            <div className="label-text">
-
-
+            <div className="label-box">
                 <img src="https://www.sephora.com/img/ufe/icons/find-store.svg"  alt='img'/>
-
-
                 <div>
                     <span>Get the App</span>
                     <p>Text "APP" to 36681</p>
                 </div>
             </div>
-            <div className="label-text">
-
-
+            <div className="label-box">
                 <img src="https://www.sephora.com/img/ufe/icons/cc-outline-ko.svg  " alt='img' />
-
-
                 <div>
                     <span> Sephora Credit Card Program
-
                     </span>
                     <p>Want 25% off your Sephora purchase1? DETAILS</p>
                 </div>
@@ -124,20 +106,26 @@ const Footer = () => {
             </div>
 
 
-            <div>
+            <div className="countries_container">
                 <h6>Region & Language</h6>
-                <p><img className="countries" src="https://www.sephora.com/img/ufe/flags/us.svg" alt='img'/> United States - English
-                </p>
-                <p><img className="countries" src="https://www.sephora.com/img/ufe/flags/ca.svg" alt='img' /> Cannada - English</p>
-                <p><img className="countries" src="https://www.sephora.com/img/ufe/flags/ca.svg"  alt='img'/> Cannada - Francais</p>
-
+                <div className="countries_box">
+                <img className="countries" src="https://www.sephora.com/img/ufe/flags/us.svg" alt='img'/>
+                <p>United States - English</p>
+                </div>
+                <div className="countries_box">
+                <img className="countries" src="https://www.sephora.com/img/ufe/flags/ca.svg" alt='img'/>
+                <p>Cannada - English</p>
+                </div>
+                <div className="countries_box">
+                <img className="countries" src="https://www.sephora.com/img/ufe/flags/ca.svg" alt='img'/>
+                <p>Cannada - Francais</p>
+                </div>
 
             </div>
-            <div className="last">
-                <h2>We Belong to
-                    Something Beautiful</h2>
-                <div className="labelInput2">
-                    <div className="labelInput">
+            <div className="footer-section-3">
+                <h2>We Belong to Something Beautiful</h2>
+                <div className="footer-section-box">
+                    <div className="footer-section-sub-box">
 
                         <label for="sign">Sign up for Sephora Emails</label>
                         <input type="text" placeholder="Enter your email address" />
@@ -149,7 +137,7 @@ const Footer = () => {
 
         </div>
         <hr />
-        <div className="last2">
+        <div className="footer-section-4">
             <div>
                 <p>© 2022 Sephora USA, Inc. All rights reserved.</p>
                 <span>Privacy Policy</span> <span>Terms of Use Accessibility</span>
