987baa85414f9cf7e36d501230cc71a1d45ffa47
Ankur982 <singh999ankur@gmail.com>
2022-11-10 05:14:36 +0530

    adding landding page different carousal-section

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
-import Footer from './components/footer/Footer';
-import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Footer/>
+      <Home/>
     </div>
   );
 }
