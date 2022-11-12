e6fc477dc60faf4611d9a84cebaeaf79439300f3
Ankur982 <singh999ankur@gmail.com>
2022-11-12 16:47:07 +0530

    updaing code

--- api/routes/product.js
+++ api/routes/product.js
@@ -60,18 +60,26 @@ router.get("/find/:id", async (req, res) => {
 router.get("/", async (req, res) => {
   const qNew = req.query.new;
   const qCategory = req.query.category;
+  const qproduct_type = req.query.product_type;
   try {
     let products;
 
     if (qNew) {
-      products = await Product.find().sort({ createdAt: -1 }).limit(1);
+      products = await Product.find().sort({ createdAt: -1 }).limit(5);
     } else if (qCategory) {
       products = await Product.find({
         categories: {
           $in: [qCategory],
         },
       });
-    } else {
+    } else if(qproduct_type){
+      products = await Product.find({
+        categories: {
+          $in: [qproduct_type],
+        },
+      });
+    }
+    else {
       products = await Product.find();
     }
 
--- client/src/context/UserAuthcontext.js
+++ client/src/context/UserAuthcontext.js
@@ -1,51 +1,51 @@
-// import { useContext } from "react";
-// import { createContext } from "react";
-// import { createUserWithEmailAndPassword ,signOut, signInWithEmailAndPassword, onAuthStateChanged, GoogleAuthProvider,signInWithPopup, sendPasswordResetEmail
-// } from "firebase/auth";
-// import { auth } from "../firebase";
-// import { useEffect } from "react";
-// import { useState } from "react";
+import { useContext } from "react";
+import { createContext } from "react";
+import { createUserWithEmailAndPassword ,signOut, signInWithEmailAndPassword, onAuthStateChanged, GoogleAuthProvider,signInWithPopup, sendPasswordResetEmail
+} from "firebase/auth";
+import { auth } from "../firebase";
+import { useEffect } from "react";
+import { useState } from "react";
 
 
-// const userAuthContext= createContext();
+const userAuthContext= createContext();
 
-// export function UserAuthContextProvider({children}){
+export function UserAuthContextProvider({children}){
 
-//     const [user,setUser]= useState("")
+    const [user,setUser]= useState("")
 
-// function signUp(email,password){
-//   return createUserWithEmailAndPassword(auth, email,password)
-// }
+function signUp(email,password){
+  return createUserWithEmailAndPassword(auth, email,password)
+}
 
-// function logIn(email,password){
-//     return signInWithEmailAndPassword(auth, email,password)
-//   }
+function logIn(email,password){
+    return signInWithEmailAndPassword(auth, email,password)
+  }
 
-//   function googleSignIn(){
-//     const provider = new GoogleAuthProvider();
-//     return signInWithPopup(auth, provider)
-//   }
+  function googleSignIn(){
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(auth, provider)
+  }
 
-//  function logOut(){
-//      return signOut(auth)
-//  } 
+ function logOut(){
+     return signOut(auth)
+ } 
 
-// useEffect(()=>{
-//   const unsubscribe= onAuthStateChanged(auth,(currentuser)=>{
-//    setUser(currentuser)
-//    });
-//    return()=>{
-//     unsubscribe()
-//    }
-// },[])
-//     return(
-//         <userAuthContext.Provider value={{user,signUp,logIn,logOut,googleSignIn}}>
-//             {children}
-//         </userAuthContext.Provider>
-//     )
+useEffect(()=>{
+  const unsubscribe= onAuthStateChanged(auth,(currentuser)=>{
+   setUser(currentuser)
+   });
+   return()=>{
+    unsubscribe()
+   }
+},[])
+    return(
+        <userAuthContext.Provider value={{user,signUp,logIn,logOut,googleSignIn}}>
+            {children}
+        </userAuthContext.Provider>
+    )
 
-// }
+}
 
-// export function useUserAuth(){
-//     return useContext(userAuthContext)
-// }
+export function useUserAuth(){
+    return useContext(userAuthContext)
+}
